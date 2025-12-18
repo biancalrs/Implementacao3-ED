15c3fb65e56e8a5aa75c82bb004e94ddc149514b
Bianca <biancalrs@gmail.com>
2025-12-17 23:26:23 -0300

    questão 3

---
+++ questao3_RedBlack.py
@@ -0,0 +1,9 @@
+RED = True
+BLACK = False
+
+class Node:
+    def __init__(self, key, color):
+        self.key = key
+        self.color = color
+        self.left = None
+        self.right = None
